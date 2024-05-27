a4e9e542e0835b9bf8d1200235a4f2cfa86da9b3
Pablo Torrez <pablotorrez90@outlook.com>
2024-05-27 01:28:43 -0300

    arreglando unos bugs que se encontraron por los Icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-
-import image from "@astrojs/image";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-
-  integrations: [
-    
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp'
-    })
-  ]
+  integrations: [icon()]
 });
