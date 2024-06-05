5cf3f9d043f15b250211fc6e68cd59c10888c98d
Pablo Torrez <pablotorrez90@outlook.com>
2024-06-05 03:02:49 -0300

    add code of the sitemap and build an RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  site: 'https://blog-article-website.netlify.app/',
+  integrations: [icon(), sitemap()]
 });
